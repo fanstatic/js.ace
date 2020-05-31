d4f7a863e8c4e1fa3cdbe6638bfe8ad9fe3aefa2
Jan-Jaap Driessen <jdriessen@minddistrict.com>
2020-05-31 15:23:29 +0200

    Update changes and tests after no longer making separate Resource objects for mode/ext/worker objects.

--- js/ace/__init__.py
+++ js/ace/__init__.py
@@ -5,12 +5,6 @@ library = Library('js.ace', 'resources')
 ace = Resource(library, 'ace.js')
 
 # All resources from ace-build subdir "src-min-noconflict" are copied into
-# this packge, but only frequently used resources are defined as Resource
-# objects.
-
-mode_html        = Resource(library, 'mode-html.js')
-mode_javascript  = Resource(library, 'mode-javascript.js')
-mode_json        = Resource(library, 'mode-json.js')
-mode_xml         = Resource(library, 'mode-xml.js')
-
-worker_javascript = Resource(library, 'worker-javascript.js')
+# this packge, but only top-level resources are defined as Resource objects.
+# Load the extra mode/keybindings/themes/workers in javascript using
+# ace.require().
